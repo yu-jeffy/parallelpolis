088f5ea5285aacd2ccc907d5dfae112b2a991a66
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-12-23 02:27:47 +0000

    app layout, styles, favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../app/globals.css';
+import '../styles/globals.css';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Parallel Polis</title>
+        <meta name="description" content="Parallel Polis Research Collective - towards agi and beyond" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
