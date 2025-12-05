038ae2751588d824b290760c9c86b9f7536f1bbf
Jeffy Yu <105130385+yu-jeffy@users.noreply.github.com>
2025-12-04 22:45:56 -0800

    x1

--- components/Footer.js
+++ components/Footer.js
@@ -12,42 +12,49 @@ const Footer = () => {
                     width={50}
                     height={50}
                     alt="openai logo"
+                    className={styles.logoImage}
                 />
                 <Image
                     src="/metaai2.png"
                     width={120}
                     height={40}
                     alt="meta ai logo"
+                    className={styles.logoImageWide}
                 />
                 <Image
                     src="/llamaIndex.png"
                     width={50}
                     height={50}
                     alt="llamaindex logo"
+                    className={styles.logoImage}
                 />
                 <Image
                     src="/langchain.png"
                     width={50}
                     height={50}
                     alt="langchain logo"
+                    className={styles.logoImage}
                 />
                 <Image
                     src="/btc.png"
                     width={50}
                     height={50}
                     alt="btc logo"
+                    className={styles.logoImage}
                 />
                 <Image
                     src="/ethereum.png"
                     width={50}
                     height={50}
                     alt="eth logo"
+                    className={styles.logoImage}
                 />
                 <Image
                     src="/solana.png"
                     width={50}
                     height={50}
                     alt="solana logo"
+                    className={styles.logoImage}
                 />
             </div>
             <span className={styles.disclaimer}>All trademarks and logos are the property of their respective owners and their use here does not imply endorsement or affiliation.</span>
